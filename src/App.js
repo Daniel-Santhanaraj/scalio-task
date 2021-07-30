5e780e6f46e5a3a0543f22e406383c980b2f0b2f
Santa <daniel.santhanaraj>
2021-07-30 14:51:30 +0530

    search and css updates

--- src/App.js
+++ src/App.js
@@ -6,28 +6,29 @@ import Results from './components/Results';
 function App() {
   const [login, setLogin] = useState('');
   const [result, setResult ] = useState([]);
-  
-  
+
   const dataCall = () => {
    // https://api.github.com/search/users?q=foo%20in:login&per_page=3
     if(login !== '') {
       let url = 'https://api.github.com/search/users?q='+login+'%20in:login';
       fetch(url)
       .then((res) => res.json())
-      .then((result) => {
-        setResult(result);
+      .then((d) => {
+        setResult(d);
       })
     }
   }
 
-  useEffect(dataCall, [login])
-  
+  useEffect(dataCall, [login]);
+
+
 
   return (
     <div className="App">
-      <p>{login}</p>
-      <Search getLogin = { login => setLogin(login) }/>
-      <Results result={result}/>
+      <div className="container">
+        <Search getLogin = { login => setLogin(login) }/>
+        <Results result={result}/>
+      </div>
     </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,51 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const result = (props) => {
+const Result = (props) => {
+    const [isSort, setSort] = useState(false)
+    var sorted = null;
+    if( props.result.length !== 0 && props.result.items.length > 0) {
+        sorted =  props.result.items.sort((a,b) => { 
+            let a_login = a.login.toLowerCase();
+            let b_login = b.login.toLowerCase();
+
+            if(isSort) {
+                if(a_login < b_login) { return -1; }
+                if(a_login > b_login) { return 1; }
+            } else {
+                if(a_login < b_login) { return 1; }
+                if(a_login > b_login) { return -1; }
+            }
+            return 0;
+        });
+    }
+
     return (
-        <div>
+        <div className="result">
             {
-                (props.result.items) ?
-                props.result.items.map((item, i) => {
+                (sorted) ? 
+                <h2 className="sort">Sort By <span onClick={() => setSort(!isSort)} className={(isSort) ? 'active': ''}>Login</span></h2>
+                : null
+            }
+           
+            <div className="sortDiv">
+            {  
+                (sorted) ?
+                sorted.map((item, i) => {
                     return (
-                        <div key={i}>
-                            <img src={item.avatar_url} alt="avatar" />
-                            <h3>{item.login}</h3>
-                            <p>{item.type}</p>
+                        <div className="cardDiv" key={i}>
+                            <div className="card">
+                                <img src={item.avatar_url} alt="avatar" />
+                                <h3>{(item.login.length > 23) ? item.login.substring(0, 25) + '...' : item.login }</h3>
+                                <p>{item.type}</p>
+                            </div>
                         </div>
                     )
                 })
-                : null
+                : <p className="no-data">No data</p>
             }
-
-            {/* <table>
-                <thead>
-                    <tr>
-                        <td>avatar_url</td>
-                        <td>login</td>
-                        <td>type</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        (props.result.items) ?
-                        props.result.items.map((item, i) => {
-                            return (
-                                <tr key={i}>
-                                    <td>
-                                        <img src={item.avatar_url} alt="avatar" /> 
-                                    </td>
-                                    <td>{item.login}</td>
-                                    <td>{item.type}</td>
-                                </tr>
-                            )
-                        })
-                        : null
-                    }
-                    
-                </tbody>
-            </table> */}
+            </div>
         </div>
     )
 }
 
-export default result;
+export default Result;
